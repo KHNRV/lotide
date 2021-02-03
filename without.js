4b03c167d3256ab3fa39a2187d9dd927142744e3
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-03 02:00:05 +0000

    minor refactor that I forgot what it was

--- without.js
+++ without.js
@@ -40,19 +40,6 @@ const assertArraysEqual = (actualArray, expectedArray) => {
 };
 
 /**
- * Compare two value and print out if they match or not
- * @param {*} actual - Enter the variable to test
- * @param {*} expected - Enter the expected value of the variable
- */
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-/**
  * This function take in a source array and a itemsToRemove array. It return a
  * new array with only those elements from source that are not present in the
  * itemsToRemove array.
