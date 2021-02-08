164280a2386d37d49a10ca142627cea572acd4e7
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-08 13:42:36 +0000

    Refractor of eqArrays.js: created seperate test file; use of module.e…

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,4 @@
-/**
- * Function that takes in two arrays and returns true or false, based on a
- * perfect match.
- * @param {array} firstArray -
- * @param {array} secondArray -
- * @returns {boolean} Return whether the arrays or identical
- */
-const eqArrays = (firstArray, secondArray) => {
-  // Compare the length of the arrays.
-  if (firstArray.length === secondArray.length) {
-    // Compare every items of the arrays idividually. Return false at the first no match.
-    for (let index in firstArray) {
-      if (firstArray[index] !== secondArray[index]) {
-        return false;
-      }
-    }
-  } else {
-    // Return false if no match
-    return false;
-  }
-  // Return true if every comparison were a match
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 /**
  * Function that takes in two arrays and print out if they match or not
@@ -39,8 +17,4 @@ const assertArraysEqual = (actualArray, expectedArray) => {
   }
 };
 
-// TEST
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
