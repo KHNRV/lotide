40d776aeebf72696e6395f21263b254e88b88560
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-08 13:32:41 +0000

    Refractor of eqArrays.js: created seperate test file; use of module.exports; spellcheck.

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,4 @@
 /**
- * Compare two value and print out if they match or not
- * @param {*} actual - Enter the variable to test
- * @param {*} expected - Enter the expected value of the variable
- */
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-/**
  * Function that takes in two arrays and returns true or false, based on a
  * perfect match.
  * @param {array} firstArray -
@@ -21,7 +8,7 @@ const assertEqual = function(actual, expected) {
 const eqArrays = (firstArray, secondArray) => {
   // Compare the length of the arrays.
   if (firstArray.length === secondArray.length) {
-    // Compare every items of the arrays idividually. Return false at the first no match.
+    // Compare every items of the arrays individually. Return false at the first no match.
     for (let index in firstArray) {
       if (firstArray[index] !== secondArray[index]) {
         return false;
@@ -35,7 +22,4 @@ const eqArrays = (firstArray, secondArray) => {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+module.exports = eqArrays;
